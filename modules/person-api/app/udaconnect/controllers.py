aa8ea853fa07fefcbd7fb55a392ad91b7db5e37c
Diogo Silvério <dblsilverio+github@gmail.com>
2021-07-07 00:39:34 -0300

    People / Location Search
    Connecting people search with connectionData gRPC Api.

--- modules/person-api/app/udaconnect/controllers.py
+++ modules/person-api/app/udaconnect/controllers.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from app.udaconnect.models import Person
+from app.udaconnect.models import Person, Location, Connection
 from app.udaconnect.schemas import ConnectionSchema, PersonSchema
-from app.udaconnect.services import PersonService, PersonProducer
+from app.udaconnect.services import PersonService, PersonProducer, ConnectionDataService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
-from typing import Any, List
+from typing import Any, List, Optional
 
 DATE_FORMAT = "%Y-%m-%d"
 
@@ -46,19 +46,39 @@ class PersonResource(Resource):
 # @api.param("end_date", "Upper bound of date range", _in="query")
 # @api.param("distance", "Proximity to a given user in meters", _in="query")
 class ConnectionDataResource(Resource):
-    # @responds(schema=Any, many=True)
-    def get(self, person_id) -> Any:
-        # start_date: datetime = datetime.strptime(
-        #     request.args["start_date"], DATE_FORMAT
-        # )
-        # end_date: datetime = datetime.strptime(
-        #     request.args["end_date"], DATE_FORMAT)
-        # distance: Optional[int] = request.args.get("distance", 5)
+    @responds(schema=ConnectionSchema, many=True)
+    def get(self, person_id) -> ConnectionSchema:
+        start_date: datetime = datetime.strptime(
+            request.args["start_date"], DATE_FORMAT
+        )
+        end_date: datetime = datetime.strptime(
+            request.args["end_date"], DATE_FORMAT)
+        distance: Optional[int] = request.args.get("distance", 5)
 
-        # results = ConnectionService.find_contacts(
-        #     person_id=person_id,
-        #     start_date=start_date,
-        #     end_date=end_date,
-        #     meters=distance,
-        # )
-        return {'hello': 'world'}
+        results = ConnectionDataService.find_contacts(
+            person_id=int(person_id),
+            start_date=start_date,
+            end_date=end_date,
+            meters=float(distance)
+        )
+
+        connection_list: List[Connection] = [
+            ConnectionDataResource.pb2_to_model(connection) for connection in results.connections
+        ]
+
+        print(connection_list)
+
+        return connection_list
+
+    @staticmethod
+    def pb2_to_model(connection) -> Connection:
+        location_pb2 = connection.location
+        location = Location(id=location_pb2.id, person_id=location_pb2.person_id,
+                            wkt_shape=location_pb2.wkt_shape,
+                            creation_time=datetime.fromtimestamp(location_pb2.creation_time.seconds))
+
+        person_pb2 = connection.person
+        person = Person(id=person_pb2.id, first_name=person_pb2.first_name,
+                        last_name=person_pb2.last_name, company_name=person_pb2.company_name)
+
+        return Connection(person=person, location=location)
