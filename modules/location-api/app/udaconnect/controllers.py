e09dad9314b245e9b67b64719bd70a96258605da
Diogo Silvério <dblsilverio+github@gmail.com>
2021-07-08 13:28:46 -0300

    Improving swagger docs for LocationAPI

--- modules/location-api/app/udaconnect/controllers.py
+++ modules/location-api/app/udaconnect/controllers.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_accepts import accepts, responds
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 
 from app.udaconnect.models.location import Location
 from app.udaconnect.schemas import LocationSchema
@@ -10,14 +10,26 @@ DATE_FORMAT = "%Y-%m-%d"
 
 api = Namespace("UdaConnect - Location API", description="Provides location data")  # noqa
 
-
-# TODO: This needs better exception handling
+location_res = api.model('Location', {
+    'id': fields.Integer,
+    'person_id': fields.Integer,
+    'longitude': fields.String,
+    'latitude': fields.String,
+    'creation_time': fields.DateTime
+})
 
 
 @api.route("/locations")
 class LocationListResource(Resource):
+
     @accepts(schema=LocationSchema)
-    @api.response(202, 'Location creation accepted')
+    @api.doc(description='Issues the creation of a new Location',
+             body=location_res,
+             responses={
+                202: 'Location creation accepted',
+                500: 'Internal server error'
+                }
+             )
     def post(self):
         location: Location = request.get_json()
 
@@ -31,6 +43,14 @@ class LocationListResource(Resource):
 class LocationResource(Resource):
 
     @responds(schema=LocationSchema)
+    @api.doc(description='Search for a given Location by its id',
+             params={'location_id': 'Required location id'},
+             responses={
+                 404: 'Location not found',
+                 500: 'Internal server error'
+                 },
+             )
+    @api.response(200, 'Location found', location_res)
     def get(self, location_id) -> Location:
         location: Location = LocationService.retrieve(location_id)
         return location
